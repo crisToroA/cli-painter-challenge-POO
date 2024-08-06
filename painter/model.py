9e30051bda8950655564c1fb0070311c4857cbc7
Cristian Toro <toroarboledacristiandavid@gmail.com>
2024-08-06 11:04:54 -0500

    Added methods area (returns the area of the circle) and method draw (draws the circle itself).

--- painter/model.py
+++ painter/model.py
@@ -1,14 +1,26 @@
+import math
 import matplotlib.pyplot as plt
 import pickle
 
+
 class Point:
-    def __init__(self, x:float, y:float):
+    def __init__(self, x: float, y: float):
         self.x: float = x
         self.y: float = y
 
+
 class Circle:
-    def __init__(self, center:Point, radius:float):
+    def __init__(self, center: Point, radius: float):
         self.center: Point = center
         self.radius: float = radius
+
+    def area(self, radius: float) -> float:
+        return math.pi * radius ** 2
+
+    def draw(self):
+        circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
+        plt.gca().add_patch(circle)
+        plt.axis("scaled")
+        plt.show()
 
 
