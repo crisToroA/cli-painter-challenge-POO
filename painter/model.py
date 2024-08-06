d7f229e4cf2b1b64f5111002778746f9a69cf617
Cristian Toro <toroarboledacristiandavid@gmail.com>
2024-08-06 10:51:12 -0500

    Added class Circle and its initializer

--- painter/model.py
+++ painter/model.py
@@ -3,5 +3,12 @@ import pickle
 
 class Point:
     def __init__(self, x:float, y:float):
-        self.x: float= x
-        self.y: float= y
+        self.x: float = x
+        self.y: float = y
+
+class Circle:
+    def __init__(self, center:Point, radius:float):
+        self.center: Point = center
+        self.radius: float = radius
+
+
